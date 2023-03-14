cfc403ab4c18f2f0d34f67b1084fe988e13f1ebc
Konstantinos Maris <mariskon02@gmail.com>
2023-03-14 22:36:38 +0200

    demo with pre tag working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import SensorDataListGroup from "./SensorDataListGroup";
 
 function App() {
+  const [sensorData, setSensorData] = useState([]);
+
+  async function getSensorData() {
+    await axios
+      .get("https://drab-plum-coati-wear.cyclic.app/api/pressure")
+      .then((res) => {
+        console.log(res.data);
+        setSensorData(res.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      await getSensorData();
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="w-100 vh-100 d-flex flex-column justify-content-center align-items-center">
+        <h1>Water Pump SB11</h1>
+        {/* <SensorDataListGroup></SensorDataListGroup> */}
+        <pre className="w-75">{JSON.stringify(sensorData, null, 4)}</pre>
+      </div>
+    </>
   );
 }
 
