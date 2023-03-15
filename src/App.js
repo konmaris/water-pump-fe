df09993c791f9d725daf23b09828561e08cdbe73
Konstantinos Maris <mariskon02@gmail.com>
2023-03-15 16:10:36 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
-import SensorDataListGroup from "./SensorDataListGroup";
-import { ListGroupItem } from "react-bootstrap";
+
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 import moment from "moment/moment";
 
 function App() {
@@ -13,7 +13,20 @@ function App() {
       .get("https://drab-plum-coati-wear.cyclic.app/api/pressure")
       .then((res) => {
         // console.log(res.data);
-        setSensorData(res.data);
+        // setSensorData(res.data);
+
+        let sum = 0;
+        res.data.forEach((x) => {
+          sum += parseFloat(x.voltage);
+        });
+
+        const avg = sum / 100;
+        // console.log({ avg });
+
+        let arr = res.data.filter((val) => val.voltage - avg > 0);
+        // console.log({ arr });
+
+        setSensorData(arr);
       })
       .catch((err) => {
         console.log(err.message);
@@ -21,9 +34,7 @@ function App() {
   }
 
   useEffect(() => {
-    const interval = setInterval(async () => {
-      await getSensorData();
-    }, 3000);
+    const interval = setInterval(getSensorData, 3000);
 
     return () => {
       clearInterval(interval);
@@ -32,20 +43,21 @@ function App() {
 
   const sensorDataListItems = sensorData.map((x) => {
     const voltage = x.voltage;
-    const offset = 0.4784;
-    const maxPressure = 5.4;
+    const offset = 0.48173;
+    const maxPressure = 5.39;
     const pressure = (voltage - offset) * 250 - 100;
-    const percentage = ((pressure / maxPressure) * 100).toFixed(2);
+    const percentage = ((pressure.toFixed(2) / maxPressure) * 100).toFixed(2);
+    const time = moment(x.createdAt).format("HH:mm:ss");
 
     return (
       <ListGroupItem key={x._id}>
-        Voltage: {voltage} Volts
+        <strong>Voltage:</strong> {voltage} Volts
         <br />
-        Pressure: {pressure} KPa
+        <strong>Pressure:</strong> {pressure.toFixed(2)} KPa
         <br />
-        Percentage: {percentage}%
+        <strong>Percentage:</strong> {percentage}%
         <br />
-        Time: {moment(x.time).format("hh:mm:ss")}
+        <strong>Time:</strong> {time}
       </ListGroupItem>
     );
   });
@@ -53,8 +65,14 @@ function App() {
   return (
     <>
       <div className="w-100 vh-100 d-flex flex-column justify-content-center align-items-center">
-        <h1>Water Pump SB11</h1>
-        <SensorDataListGroup>{sensorDataListItems}</SensorDataListGroup>
+        <h1 className="mb-4">Water Pump SB11</h1>
+        <h5>
+          Showing {sensorData.length} of 100 results from database. These were
+          considered accurate.
+        </h5>
+        <ListGroup className="h-75 w-50" style={{ overflowY: "auto" }}>
+          {sensorDataListItems}
+        </ListGroup>
       </div>
     </>
   );
