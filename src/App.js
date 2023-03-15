08924f14a19fc4f6b715452bd42bf9abd4af623f
Konstantinos Maris <mariskon02@gmail.com>
2023-03-15 02:57:11 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 import SensorDataListGroup from "./SensorDataListGroup";
+import { ListGroupItem } from "react-bootstrap";
+import moment from "moment/moment";
 
 function App() {
   const [sensorData, setSensorData] = useState([]);
@@ -10,7 +12,7 @@ function App() {
     await axios
       .get("https://drab-plum-coati-wear.cyclic.app/api/pressure")
       .then((res) => {
-        console.log(res.data);
+        // console.log(res.data);
         setSensorData(res.data);
       })
       .catch((err) => {
@@ -28,12 +30,31 @@ function App() {
     };
   }, []);
 
+  const sensorDataListItems = sensorData.map((x) => {
+    const voltage = x.voltage;
+    const offset = 0.4784;
+    const maxPressure = 5.4;
+    const pressure = (voltage - offset) * 250 - 100;
+    const percentage = ((pressure / maxPressure) * 100).toFixed(2);
+
+    return (
+      <ListGroupItem key={x._id}>
+        Voltage: {voltage} Volts
+        <br />
+        Pressure: {pressure} KPa
+        <br />
+        Percentage: {percentage}%
+        <br />
+        Time: {moment(x.time).format("hh:mm:ss")}
+      </ListGroupItem>
+    );
+  });
+
   return (
     <>
       <div className="w-100 vh-100 d-flex flex-column justify-content-center align-items-center">
         <h1>Water Pump SB11</h1>
-        {/* <SensorDataListGroup></SensorDataListGroup> */}
-        <pre className="w-75">{JSON.stringify(sensorData, null, 4)}</pre>
+        <SensorDataListGroup>{sensorDataListItems}</SensorDataListGroup>
       </div>
     </>
   );
--- src/SensorDataListGroup.jsx
+++ src/SensorDataListGroup.jsx
@@ -2,12 +2,8 @@ import ListGroup from "react-bootstrap/ListGroup";
 
 function SensorDataListGroup({ children }) {
   return (
-    <ListGroup>
-      <ListGroup.Item>Cras justo odio</ListGroup.Item>
-      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-      <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    <ListGroup className="h-75 w-50" style={{ overflowY: "auto" }}>
+      {children}
     </ListGroup>
   );
 }
